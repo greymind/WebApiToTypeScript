0675e50f7c202afb430561fd282a6e33cd6eb5c8
Balakrishnan (Balki) Ranganathan <balki@live.com>
2016-08-21 16:13:58 +0200

    Test framework for hitting endpoints

--- src/WebApiTestApplication/Scripts/Test.ts
+++ src/WebApiTestApplication/Scripts/Test.ts
@@ -1,2 +1,24 @@
-var xx = new Endpoints.TestEndpoint.GetSomething(1, 2);
-console.log(xx.id, xx.y, xx.toString());
+declare var $: JQueryStatic;
+import TestEndpoint = Endpoints.TestEndpoint;
+
+var endpoints: any[] = [
+    new TestEndpoint.Get("cap"),
+    new TestEndpoint.Get1(7, "cap"),
+    new TestEndpoint.GetSomething("cap", 1, 2),
+    new TestEndpoint.GetSomethingElse(3, "clap", "cap"),
+    new TestEndpoint.Post("cap", "balki"),
+    new TestEndpoint.Put(5, "boo", "cap"),
+    new TestEndpoint.Delete(2, "cap")
+];
+
+endpoints.forEach(e => {
+    let endpoint = e.toString();
+    console.log(endpoint);
+
+    $.ajax({
+        method: e.verb,
+        url: endpoint
+    }).then((data, status) => {
+        console.log(e.verb, endpoint, status, data);
+    });
+});
