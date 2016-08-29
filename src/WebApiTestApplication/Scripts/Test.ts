e05d11c369ba093bfe3d1bf5f0c67c23231171b9
Balakrishnan Ranganathan <branganathan@neogov.net>
2016-08-29 12:27:46 +0200

    Format test file

--- src/WebApiTestApplication/Scripts/Test.ts
+++ src/WebApiTestApplication/Scripts/Test.ts
@@ -10,10 +10,34 @@ c.Name = "bappy";
 c.Number = 25;
 testObject.C = c;
 
-new Endpoints.Test.Get("cap").call().then(responsePrinter);
-new Endpoints.Test.Get1("77", "cap").call().then(responsePrinter);
-new Endpoints.Test.GetSomething(7, "cap", 2).call().then(responsePrinter);
-new Endpoints.Test.GetSomethingElse(3, testObject, "cap").call().then(responsePrinter);
-new Endpoints.Test.Post("cap").call(testObject).then(responsePrinter);
-new Endpoints.Test.Put(5, "cap").call("data").then(responsePrinter);
-new Endpoints.Test.Delete(2, "cap").call().then(responsePrinter);
+new Endpoints.Test.Get("cap")
+    .call()
+    .then(responsePrinter);
+
+new Endpoints.Test.Get1("77", "cap")
+    .call()
+    .then(responsePrinter);
+
+new Endpoints.Test.GetSomething(7, "cap", 2)
+    .call()
+    .then(responsePrinter);
+
+new Endpoints.Test.GetSomethingElse(3, testObject, "cap")
+    .call()
+    .then(responsePrinter);
+
+new Endpoints.Test.Post("cap")
+    .call(null)
+    .then(responsePrinter);
+
+new Endpoints.Test.Post("cap")
+    .call(testObject)
+    .then(responsePrinter);
+
+new Endpoints.Test.Put(5, "cap")
+    .call("data")
+    .then(responsePrinter);
+
+new Endpoints.Test.Delete(2, "cap")
+    .call()
+    .then(responsePrinter);
