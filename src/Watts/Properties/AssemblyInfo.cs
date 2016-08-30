85283a18ae3e7622f17f73fdcbc305e6e616fd8e
Balakrishnan Ranganathan <branganathan@neogov.net>
2016-08-30 16:19:36 +0200

    Spruce up assembly info ;)

--- src/Watts/Properties/AssemblyInfo.cs
+++ src/Watts/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Watts")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A tool for autogenerating endpoints, enums and interfaces from Web API controllers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Greymind")]
 [assembly: AssemblyProduct("Watts")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016 Balakrishnan (Balki) Ranganathan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
