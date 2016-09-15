f5cb57776e3d896d3bf0dc80774a019383b332c5
Balakrishnan (Balki) Ranganathan <balki@live.com>
2016-09-15 20:26:03 +0200

    Added MegaClass and Post endpoint for test

--- src/WebApiTestApplication/Controllers/ThingyController.cs
+++ src/WebApiTestApplication/Controllers/ThingyController.cs
@@ -3,6 +3,11 @@ using Newtonsoft.Json;
 
 namespace WebApiTestApplication.Controllers
 {
+    public class MegaClass : AnotherClass
+    {
+        public int Something { get; set; }
+    }
+
     [RoutePrefix("api/thingy")]
     public class ThingyController : ApiController
     {
@@ -15,7 +20,7 @@ namespace WebApiTestApplication.Controllers
 
         [HttpGet]
         [Route("{id}")]
-        public string Get(int? id, string x, [FromUri]AnotherClass c)
+        public string Get(int? id, string x, [FromUri]MegaClass c)
         {
             var valueJson = JsonConvert.SerializeObject(c);
             return $"value {id} {x} {valueJson}";
@@ -26,6 +31,14 @@ namespace WebApiTestApplication.Controllers
         public string Getty(string x, int y)
         {
             return $"value {x} {y}";
+        }
+
+        [HttpPost]
+        [Route("")]
+        public string Post(MegaClass value)
+        {
+            var valueJson = JsonConvert.SerializeObject(value);
+            return $"thanks for the {valueJson} in the ace!";
         }
     }
 }
