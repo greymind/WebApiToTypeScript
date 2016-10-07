3a0a3524545be6cbe35aff7e1b6622910ae83a7d
Balakrishnan (Balki) Ranganathan <balki@live.com>
2016-10-07 11:40:51 +0200

    Pack and push now rebuilds release as well

--- PackAndPush.ps1
+++ PackAndPush.ps1
@@ -7,8 +7,12 @@ function Activate() {
     $confirmation = Read-Host -Prompt "Are you sure you want to push v$NuspecVersion (y/n)"
     if (!($confirmation.ToLower() -eq "y"))
     {
-        return;
+        return
     }
+
+    $msbuildPath = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+    $solutionPath = (Resolve-Path ".\src\Watts\Watts.csproj")
+    & $msbuildPath /t:Build /m /nologo /verbosity:quiet /p:Configuration=Release $solutionPath
 
     nuget pack .\Watts.nuspec
     nuget push ".\WebApiToTypeScript.$NuspecVersion.nupkg" -Source https://www.nuget.org/api/v2/package
