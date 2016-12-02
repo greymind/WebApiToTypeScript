b0d7513346996f29cc4a374be8a74f5abfda1abf
Balakrishnan (Balki) Ranganathan <balki@live.com>
2016-12-02 18:46:04 +0100

    Updated script to check after msbuild now

--- PackAndPush.ps1
+++ PackAndPush.ps1
@@ -4,15 +4,15 @@ param(
 )
 
 function Activate() {
+    $msbuildPath = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+    $solutionPath = (Resolve-Path ".\src\Watts\Watts.csproj")
+    & $msbuildPath /t:Build /m /nologo /verbosity:quiet /p:Configuration=Release $solutionPath
+
     $confirmation = Read-Host -Prompt "Are you sure you want to push v$NuspecVersion (y/n)"
     if (!($confirmation.ToLower() -eq "y"))
     {
         return
     }
-
-    $msbuildPath = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
-    $solutionPath = (Resolve-Path ".\src\Watts\Watts.csproj")
-    & $msbuildPath /t:Build /m /nologo /verbosity:quiet /p:Configuration=Release $solutionPath
 
     nuget pack .\Watts.nuspec
     nuget push ".\WebApiToTypeScript.$NuspecVersion.nupkg" -Source https://www.nuget.org/api/v2/package
