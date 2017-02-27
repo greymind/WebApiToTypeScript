0eae25c601e1c98501130af5d4f3eb87c4e837d0
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-02-27 09:44:26 +0100

    Update pack and push script to use nuspec version from .nuspec directly

--- PackAndPush.ps1
+++ PackAndPush.ps1
@@ -1,7 +1,4 @@
-param(
-    [Parameter(Mandatory = $true)]
-    [string] $NuspecVersion
-)
+param()
 
 function Activate() {
     $msbuildPath = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
@@ -10,14 +7,18 @@ function Activate() {
     Write-Host "Building release $solutionPath..."
     & $msbuildPath /t:Build /m /nologo /verbosity:quiet /p:Configuration=Release $solutionPath
 
-    $confirmation = Read-Host -Prompt "Are you sure you want to push v$NuspecVersion (y/n)"
+    $nuspecPath = (Resolve-Path ".\Watts.nuspec").Path
+    [xml]$nuspecXml = Get-Content $nuspecPath
+    $version = $nuspecXml.package.metadata.version
+
+    $confirmation = Read-Host -Prompt "Are you sure you want to push v$version (y/n)"
     if (!($confirmation.ToLower() -eq "y"))
     {
         return
     }
 
     nuget pack .\Watts.nuspec
-    nuget push ".\WebApiToTypeScript.$NuspecVersion.nupkg" -Source https://www.nuget.org/api/v2/package
+    nuget push ".\WebApiToTypeScript.$version.nupkg" -Source https://www.nuget.org/api/v2/package
     
     Remove-Item *.nupkg -Force
 }
