2268b4823f1134794c6d8d3c6160b227668c0844
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-07-21 15:32:20 +0200

    Update pack and push script

--- PackAndPush.ps1
+++ PackAndPush.ps1
@@ -1,20 +1,30 @@
 param()
 
 function Activate() {
-    $msbuildPath = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
-    $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+    $MSBuildPaths = @(
+        "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe",
+        "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe",
+        "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+    )
+
+    for ($i = 0; $i -lt $MSBuildPaths.Count; $i++) {
+        $MSBuildPath = $MSBuildPaths[$i];
+        if (Test-Path $MSBuildPath -ErrorAction SilentlyContinue) {
+            break;
+        }
+    }
+
     $solutionPath = (Resolve-Path ".\src\Watts\Watts.csproj")
 
     Write-Host "Building release $solutionPath..."
-    & $msbuildPath /t:Build /m /nologo /verbosity:quiet /p:Configuration=Release $solutionPath
+    & $MSBuildPath /t:Build /m /nologo /verbosity:quiet /p:Configuration=Release $solutionPath
 
     $nuspecPath = (Resolve-Path ".\Watts.nuspec").Path
     [xml]$nuspecXml = Get-Content $nuspecPath
     $version = $nuspecXml.package.metadata.version
 
     $confirmation = Read-Host -Prompt "Are you sure you want to push v$version (y/n)"
-    if (!($confirmation.ToLower() -eq "y"))
-    {
+    if (!($confirmation.ToLower() -eq "y")) {
         return
     }
 
