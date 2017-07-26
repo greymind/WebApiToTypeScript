c9eaa3a7425baa2b71aefaa3a8b7b0779a430cb8
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-07-26 14:01:39 +0200

    Read json property name attible on type's things

--- src/WebApiToTypeScript/Types/MemberWithCSharpType.cs
+++ src/WebApiToTypeScript/Types/MemberWithCSharpType.cs
@@ -1,8 +1,12 @@
-﻿namespace WebApiToTypeScript.Types
+﻿using Mono.Cecil;
+using System.Collections.Generic;
+
+namespace WebApiToTypeScript.Types
 {
     public class MemberWithCSharpType
     {
         public string Name { get; set; }
         public CSharpType CSharpType { get; set; }
+        public List<CustomAttribute> CustomAttributes { get; set; }
     }
 }
