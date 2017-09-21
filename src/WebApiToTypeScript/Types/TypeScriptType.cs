b385c0b605a2a4f15ee4088c807495ab5cbc4678
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-09-21 17:55:38 +0200

    Indicate if it is a custom mapped type

--- src/WebApiToTypeScript/Types/TypeScriptType.cs
+++ src/WebApiToTypeScript/Types/TypeScriptType.cs
@@ -4,9 +4,12 @@
     {
         public string TypeName { get; set; }
         public string InterfaceName { get; set; }
+
         public bool IsEnum { get; set; }
         public bool IsPrimitive { get; set; }
 
         public int CollectionLevel { get; set; }
+
+        public bool IsMappedType { get; set; }
     }
 }
